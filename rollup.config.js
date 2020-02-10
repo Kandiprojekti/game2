48cde825bf9d51409d38961aba1ddd60e2c33899
svenskapojkarna <hytonenaleksi@gmail.com>
2020-02-10 10:58:52 +0200

    Changed name to game2

--- rollup.config.js
+++ rollup.config.js
@@ -28,10 +28,10 @@ export const build = [
 ];
 
 export const dev = {
-    input: 'testipeli.js',
+    input: 'game2.js',
     output: {
         name: 'kiigame',
-        file: 'public/testipeli.js',
+        file: 'public/game2.js',
         format: 'iife',
         sourcemap: true
     },
